30c0a047a51edd7ff36fe676142c5c620afbebc0
HeavenlyDark <106950115+HeavenlyDark@users.noreply.github.com>
2023-05-30 17:39:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,24 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-  return render_template('index.html')
+def home():
+    return render_template('home.html')
+
+@app.route('/texture-pack')
+def texturepack():
+    return render_template('texture-pack.html')
+
+@app.route('/youtube')
+def youtube():
+    return render_template('youtube.html')
+
+@app.route('/portales')
+def portales():
+    return render_template('portales.html')
+
+@app.route('/slime-chunks')
+def slimechunks():
+    return render_template('slime-chunks.html')
 
 if __name__ == '__main__':
   app.run(port=5000)
